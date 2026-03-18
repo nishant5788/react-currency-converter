f9d965be64e8e0c71cf9b40e20c66b3f7b24bcf8
Nishant Sharma <nishants5788@gmail.com>
2026-03-19 03:41:45 +0800

    Enhance currency fetching: add error handling and improve loading state management

--- src/hooks/useCurrency.js
+++ src/hooks/useCurrency.js
@@ -4,26 +4,50 @@ export default function useCurrency (amount, fromCur, toCur) {
 
     const [converted, setConverted] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
   
     useEffect(function() {
             
-            const controller = new AbortController();
-            
+            const controller = new AbortController();   
+
             async function fetchCurrentRate() {
+               try {
                 
                 setIsLoading(true);
+      setError("");
                 
                 const res = await fetch(
               `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCur}`, { signal: controller.signal }
             );
+
+            if(!res.ok) throw new Error("Something went wrong with Fetching Currencies!");
                 
                 const data = await res.json();
+
+                if(data.Response === "False") throw new Error ("Response is not Found!")
                 
                 setConverted(data.rates[toCur]);
                 
+                setError("");
                 setIsLoading(false);
                 
             }
+
+            catch(err) {
+              console.log("err is" + err);
+
+              if(err.name !== "AbortError") {
+             setError(err.message);
+      }
+
+            }
+
+            finally {
+      setIsLoading(false);
+    }
+
+            }
+            
             
             fetchCurrentRate();
             
